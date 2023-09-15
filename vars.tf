183ebd83887533866c77737570c69c159e4b3c3d
staggerboy <sb@pop-os.localdomain>
2023-09-14 21:46:05 -0500

    upload offline work
    terraform and ansible

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "environment" {
+  default = "dev"
+}
+
+variable "name" {
+  default = "mywebserver"
+}
+
+variable "az" {
+  default = "us-east-1a"
+}
+
+variable "vpc_id" {
+  default = ""
+}
